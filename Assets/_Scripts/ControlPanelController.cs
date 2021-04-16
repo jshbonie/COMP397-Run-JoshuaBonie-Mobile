5df621a4e15f1586590d17ff2207c61fbb4648fc
Bonie <jshbonie@gmail.com>
2021-04-16 19:16:52 -0400

    Final Push, fixed save function to include saving the players rotation

--- Assets/_Scripts/ControlPanelController.cs
+++ Assets/_Scripts/ControlPanelController.cs
@@ -122,6 +122,7 @@ public class ControlPanelController : MonoBehaviour
     {
         player.controller.enabled = false;
         player.transform.position = sceneData.playerPosition;
+        player.transform.rotation = sceneData.playerRotation;
         player.controller.enabled = true;
 
         player.health = sceneData.playerHealth;
@@ -132,6 +133,7 @@ public class ControlPanelController : MonoBehaviour
     public void OnSaveButtonPressed()
     {
         sceneData.playerPosition = player.transform.position;
+        sceneData.playerRotation = player.transform.rotation;
         sceneData.playerHealth = player.health;
     }
 }
